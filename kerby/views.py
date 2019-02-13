53baa6cff998ffc355004cb3b0e518d5c302d961
seal8792 <kerby7425@gmail.com>
2019-02-13 11:57:29 +0900

    Delete views.py

--- kerby/views.py
+++
@@ -1,9 +0,0 @@
-import random
-from django.http import HttpResponse
-
-def lotto_numbers(request):
-    lotto_list = []
-    for i in range(1, 6):
-        lotto_list.append(random.sample(45, 6))
-        html = "<html/> No.{} : %s <html/>".format(i) % lotto_list
-    return HttpResponse(html)
