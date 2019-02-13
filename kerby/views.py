fb3701093edca8002599c79dbbfd20a02c7ac638
seal8792 <kerby7425@gmail.com>
2019-02-13 11:26:25 +0900

    Update views.py

--- kerby/views.py
+++ kerby/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 import random
+from django.http import HttpResponse
 
-def lotto_numbers(request, times) :
+def lotto_numbers(request):
     lotto_list = []
-    for i in range(1,6) :
-        lotto_list.append(random.sample(45,6))
+    for i in range(1, 6):
+        lotto_list.append(random.sample(45, 6))
         html = "<html/> No.{} : %s <html/>".format(i) % lotto_list
     return HttpResponse(html)
