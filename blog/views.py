f4ad17ee7bdb31421c9ab242d9a2cd1deaaa8116
seal8792 <kerby7425@gmail.com>
2019-02-13 11:03:05 +0900

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def articles_by_year(request, year):
+    return HttpResponse(f'''
+        {year}년도에 대한 목록
+    ''')
+
 def index(request):
     return render(request, 'blog/index.html')
 
