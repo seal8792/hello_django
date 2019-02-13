a3bc90fc62f41c4f8aa60843340310e6c802456d
seal8792 <kerby7425@gmail.com>
2019-02-13 11:56:16 +0900

    Create urls.py

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+from django.urls import register_converter
+from blog.views import index, hello_times, articles_by_year
+from blog.converters import FourDigitYearConverter
+
+register_converter(FourDigitYearConverter, 'year')
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('articles/<year:year>/', articles_by_year),
+    path('hello_times/<int:times>/', hello_times),
+    path('', index),
+    ]
