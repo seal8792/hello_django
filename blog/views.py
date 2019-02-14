b3e5d608bc43da4e496ea0d805313f5e68855455
seal8792 <kerby7425@gmail.com>
2019-02-14 10:21:27 +0900

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -32,3 +32,22 @@ def lotto_numbers(request):
         html = "<html><body>No.{} : %s </body></html>".format(i) % lotto_list
         html_list.append(html)
     return HttpResponse(html_list)
+
+def naver_blog_search(request):
+    query = request.GET.get('query')
+    if query:
+        # text = f'(query) 검색할거야.'
+        url = "http://naver.com"
+        response = requests.get(url)
+        html = response.text
+
+        soup = BeautifulSoup(html, 'html.parser')
+        tag_list = soup.select('.PM_CL_realtimeKeyword_rolling .ah_k')
+
+        keyword_list = []
+        for tag in tag_list:
+            keyword_list.append(tag.text)
+        text = keyword_list
+    else:
+        text = '검색어를 지정해 주세요.'
+    return HttpResponse(text)
