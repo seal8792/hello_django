d0bc783e9b4d5d56acc09e7a53e0265fe15365d2
seal8792 <kerby7425@gmail.com>
2019-02-14 09:01:24 +0900

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from django.urls import register_converter
-from blog.views import index, hello_times, articles_by_year
+from blog.views import index, hello_times, articles_by_year, lotto_numbers
 from blog.converters import FourDigitYearConverter
 
 register_converter(FourDigitYearConverter, 'year')
@@ -11,6 +11,7 @@ app_name = 'blog'
 urlpatterns = [
     path('articles/<year:year>/', articles_by_year),
     path('hello_times/<int:times>/', hello_times),
-    path('articles/<year:year>', articles_by_year),
+    path('articles/<year:year>/', articles_by_year),
+    path('lotto_numbers/<int:numbers>/', lotto_numbers),
     path('', index),
     ]
