6e921832364108ad443f494570b9f8079a158dbb
seal8792 <kerby7425@gmail.com>
2019-02-14 09:09:01 +0900

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -11,7 +11,7 @@ app_name = 'blog'
 urlpatterns = [
     path('articles/<year:year>/', articles_by_year),
     path('hello_times/<int:times>/', hello_times),
-    path('articles/<year:year>/', articles_by_year),
-    path('lotto_numbers/<int:numbers>/', lotto_numbers),
+    path('articles/<year:year>', articles_by_year),
+    path('lotto_numbers/', lotto_numbers),
     path('', index),
     ]
