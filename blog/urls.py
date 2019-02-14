378e2e3f0a62633a7a3298e2e97bdf6f231fd7de
seal8792 <kerby7425@gmail.com>
2019-02-14 10:22:51 +0900

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import register_converter
 from blog.views import index, hello_times, articles_by_year, lotto_numbers
 from blog.views import naver_realtime_keywords
 from blog.converters import FourDigitYearConverter
+from blog.views import naver_blog_search
 
 register_converter(FourDigitYearConverter, 'year')
 
@@ -16,4 +17,5 @@ urlpatterns = [
     path('lotto_numbers/', lotto_numbers),
     path('', index),
     path('naver/실시간검색어/', naver_realtime_keywords),
+    path('naver_blog_search/', naver_blog_search)
     ]
