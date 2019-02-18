55abfc394f6e3e0740278211cc4a292767cf9aa8
seal8792 <kerby7425@gmail.com>
2019-02-18 15:18:43 +0900

    Shop App 생성

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Shop
+from .models import Shop, Item
 
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
@@ -10,3 +10,8 @@ class ShopAdmin(admin.ModelAdmin):
     def desc(self, post):
         return post.content[:20] + '...'
 
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'desc', 'price', 'is_public']
+    list_display_link = ['name', 'price']
+    search_fields = ['name']
