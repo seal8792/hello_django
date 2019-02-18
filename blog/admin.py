4d2272dc29ce2c7054c7f121a07174bf88df1cf0
seal8792 <kerby7425@gmail.com>
2019-02-18 11:12:49 +0900

    장고 admin을 통한 데이터 관리

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'short_content', 'is_publish', 'tags', 'created_at']
+    list_display_links = ['title']
+    list_filter = ['is_publish']
+    search_fields = ['title']
+
+    def short_content(self, post):
+        return post.content[:20] + '...'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,17 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    tags = models.CharField(max_length=20)
+    tags = models.CharField(max_length=20)     # 필수 필드, migrate 시 질문 발생
+    is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
+
 class Comment(models.Model):
-    author_name = models.CharField(max_length=100)
+    author_name = models.CharField(max_length=100, db_index=True) # db_index : 필요한 필드에만 적용(migrate 필수)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
