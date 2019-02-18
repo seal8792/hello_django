28c6520c8e2e69912a21c95a8f774c19e2465784
seal8792 <kerby7425@gmail.com>
2019-02-18 11:57:23 +0900

    blog.Post/Comment 모델에 작성자 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.conf import settings
+# from django.contrib.auth.models import User # 추천하지 않는 방식
+
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)   # ('auth.User')로 사용해도 되지만....
     title = models.CharField(max_length=100)
     content = models.TextField()
     tags = models.CharField(max_length=20)     # 필수 필드, migrate 시 질문 발생
@@ -13,6 +17,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author_name = models.CharField(max_length=100, db_index=True) # db_index : 필요한 필드에만 적용(migrate 필수)
     message = models.TextField()
