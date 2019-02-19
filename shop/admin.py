b7348c4d0c485d652f8a6af15a3b4a8b441fb6f5
seal8792 <kerby7425@gmail.com>
2019-02-19 09:27:03 +0900

    shop app 수정 보완

--- shop/admin.py
+++ shop/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from .models import Shop, Item
 
-@admin.register(Shop)
+@admin.register(Shop)             # 장식자 : @는 함수나 클래스를 wrapping 하는 기능
 class ShopAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'desc', 'address']
     list_display_links = ['name']
     search_fields = ['name']
 
-    def desc(self, post):
-        return post.content[:20] + '...'
+    # def desc(self, post):
+    #     return post.desc[:20] + '...'
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
@@ -16,5 +16,5 @@ class ItemAdmin(admin.ModelAdmin):
     list_display_links = ['name']
     search_fields = ['name']
 
-    def desc(self, post):
-        return post.content[:20] + '...'
+    # def desc(self, desc):
+    #     return desc.content[:20] + '...'
--- shop/models.py
+++ shop/models.py
@@ -1,15 +1,25 @@
 from django.db import models
-from django.conf import settings
 
 class Shop(models.Model):
     name = models.CharField(max_length=100)
-    desc = models.TextField()
-    address = models.CharField(max_length=50)
+    desc = models.TextField()  # 미기입시 False 지정으로 반드시 기입해야 하는 공간이 됨
+    address = models.CharField(blank=True, max_length=50)   # 필수공간이 아님
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 class Item(models.Model):
-    shop = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # shop = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)   # 하나의 아이템은 무조건 하나의 샵에 포함
     name = models.CharField(max_length=100)
-    desc = models.TextField()
+    desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
     is_public = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     # photo
+
+    def __str__(self):
+        return self.name
